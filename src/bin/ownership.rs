14c1a37a1d02fe2fbace4d3efb215fb491f9cb36
WangDang <wangdang@sudo.ai>
2026-05-17 12:36:27 +0800

    update ownership

--- src/bin/ownership.rs
+++ src/bin/ownership.rs
@@ -3,23 +3,67 @@
 // 3. 打印 s2
 // 4. （故意尝试打印 s1）
 
-fn str_clone()
+// fn str_clone()
+// {
+//   let s1 = String::from("hello");
+//   let s2 = s1.clone();
+//   println!("s1 is {}, s2 is{}", s1,s2); 
+// }
+
+// fn str_withoutclone()
+// {
+//   let s1 = String::from("hello");
+//   let s2 = s1;
+//   println!(" s2 is{}", s2); 
+// //   println!(" s1 is{}", s1); //error
+// }
+
+// fn main()
+// {
+//    str_clone();
+//    str_withoutclone();
+// }
+
+fn take_ownership(some_string: String)
 {
-  let s1 = String::from("hello");
-  let s2 = s1.clone();
-  println!("s1 is {}, s2 is{}", s1,s2); 
+  println!("some_string is {}", some_string);
 }
 
-fn str_withoutclone()
+fn make_copy(some_integrater: i32)
 {
-  let s1 = String::from("hello");
-  let s2 = s1;
-  println!(" s2 is{}", s2); 
-//   println!(" s1 is{}", s1); //error
+  println!("some var is {}", some_integrater);
+}
+
+fn ownership1()
+{
+  let s = String::from("hello, world!");
+  take_ownership(s);
+  let x : &str = "test";
+  take_ownership(x.to_string());//创建新的，ele然后move ownership
+  let y = 5;
+  make_copy(y);
+}
+
+fn give_ownership() -> String
+{
+  let x = String::from("hello_world");
+  x
+}
+
+fn take_and_giveback(some_string : String) -> String
+{
+  some_string
+}
+
+fn ownership2()
+{
+  let str = give_ownership();
+  println!("get ownership is {}", str);
+  let s2 = take_and_giveback(str);
 }
 
 fn main()
 {
-   str_clone();
-   str_withoutclone();
+  ownership1();
+  ownership2();
 }
